3d6147f0ab0098183765ac65bbca2af4add5cfcd
Ricardo  Linck <ricardo.linck@eldorado.org.br>
2016-12-16 13:06:23 -0200

    Premium version :D

--- js/app.js
+++ js/app.js
@@ -2,12 +2,24 @@
     var app = angular.module('catApp',[]);
 
     app.controller('CatController', function(){
-        this.cats = [new Cat('John'), new Cat('Mike')];
+        this.cats = [
+            new Cat('John', 'image/octocat.png'),
+            new Cat('Mike', 'image/octocat2.jpg'),
+            new Cat('Robert', 'image/octocat3.jpg'),
+            new Cat('Dave', 'image/octocat4.png'),
+            new Cat('Peter', 'image/octocat5.png')
+         ];
+        this.selectedCat = this.cats[0];
+        this.selectCat = function(index){
+            this.selectedCat = this.cats[index];
+        }
     });
 
-    function Cat(name){
+    function Cat(name, imageSource){
         this.clicks = 0;
         this.name = name;
+        this.imageSource = imageSource;
+
         this.click = function(){
             this.clicks++;
         }
