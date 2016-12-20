419d4a7c713401eb02a5eef71cb757f55e6ee169
Ricardo  Linck <ricardo.linck@eldorado.org.br>
2016-12-20 09:08:14 -0200

    Refactoring the model and controller.

--- js/app.js
+++ js/app.js
@@ -2,26 +2,29 @@
     var app = angular.module('catApp',[]);
 
     app.controller('CatController', function(){
-        this.cats = [
+        this.cats = model;
+        this.selectedCat = this.cats[0];
+
+        this.selectCat = function(index){
+            this.selectedCat = this.cats[index];
+        }
+
+        this.incrementCatCounter = function(cat){
+            cat.clickCounter++;
+        }
+    });
+
+    var model = [
             new Cat('John', 'image/octocat.png'),
             new Cat('Mike', 'image/octocat2.jpg'),
             new Cat('Robert', 'image/octocat3.jpg'),
             new Cat('Dave', 'image/octocat4.png'),
             new Cat('Peter', 'image/octocat5.png')
          ];
-        this.selectedCat = this.cats[0];
-        this.selectCat = function(index){
-            this.selectedCat = this.cats[index];
-        }
-    });
 
     function Cat(name, imageSource){
-        this.clicks = 0;
+        this.clickCounter = 0;
         this.name = name;
         this.imageSource = imageSource;
-
-        this.click = function(){
-            this.clicks++;
-        }
     }
 }());
