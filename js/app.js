5cd563e75794f41ecaff5ee9fc653f9fb45167c8
Ricardo  Linck <ricardo.linck@eldorado.org.br>
2016-12-20 09:44:00 -0200

    Adding the admin mode.

--- js/app.js
+++ js/app.js
@@ -4,13 +4,33 @@
     app.controller('CatController', function(){
         this.cats = model;
         this.selectedCat = this.cats[0];
+        this.selectedCatAdmin;
+        this.isAdminModeVisible = false;
 
         this.selectCat = function(index){
             this.selectedCat = this.cats[index];
+            this.isAdminModeVisible = false;
+            this.selectedCatAdmin = null;
         }
 
         this.incrementCatCounter = function(cat){
             cat.clickCounter++;
+        }
+
+        this.showAdminMode = function(cat){
+            this.isAdminModeVisible = true;
+            this.selectedCatAdmin = angular.copy(cat);
+        }
+
+        this.saveAdminMode = function(){
+            this.selectedCat.name = this.selectedCatAdmin.name;
+            this.selectedCat.imageSource = this.selectedCatAdmin.imageSource;
+            this.selectedCat.clickCounter = this.selectedCatAdmin.clickCounter;
+            this.isAdminModeVisible = false;
+        }
+
+        this.cancelAdminMode = function(){
+            this.isAdminModeVisible = false;
         }
     });
 
